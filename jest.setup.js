20b3976170930c2ae7e3772a80c3fa12d264a3c6
Ayo 🖥️ <dejiodetayo@gmail.com>
2024-10-02 19:13:01 +0100

    chore : add jest config and update modules

---
+++ jest.setup.js
@@ -0,0 +1,8 @@
+// // Node configuration
+// process.env.NODE_ENV = "development";
+// process.env.PORT = 8081;
+// process.env.POSTGRES_HOST=localhost
+// process.env.POSTGRES_PORT=5432
+// process.env.POSTGRES_USER=
+// process.env.POSTGRES_PASSWORD=
+// process.env.POSTGRES_DB=
