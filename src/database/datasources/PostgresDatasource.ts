2027a22c35421657f27224e4a1eb651744a534ce
Ayo 🖥️ <dejiodetayo@gmail.com>
2024-10-02 18:54:25 +0100

    chore: general setup for default configs

--- src/database/datasources/PostgresDatasource.ts
+++ src/database/datasources/PostgresDatasource.ts
@@ -1,34 +1,42 @@
-import {registerProvider} from "@tsed/di";
-import {DataSource} from "typeorm";
-import {Logger} from "@tsed/logger";
+import { registerProvider } from "@tsed/di";
+import { DataSource } from "typeorm";
+import { Logger } from "@tsed/logger";
+import { User } from "../entities/User";
+import { Statements } from "../entities/IncomeExpenditureStatement";
+import { TransactionEntry } from "../entities/TransactionEntry";
+import dotenv from "dotenv";
 
-export const PostgresDatasource = Symbol.for("PostgresDatasource");
+dotenv.config();
+
+export const POSTGRES_DATA_SOURCE = Symbol.for("PostgresDatasource");
 export type PostgresDatasource = DataSource;
-export const postgresDatasource = new DataSource({
+export const PostgresDatasource = new DataSource({
   type: "postgres",
-  entities: [],
-  host: "localhost",
-  port: 5432,
-  username: "test",
-  password: "test",
-  database: "test"
+  entities: [User, Statements, TransactionEntry],
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB
 });
 
-
 registerProvider<DataSource>({
-  provide: PostgresDatasource,
+  provide: POSTGRES_DATA_SOURCE,
   type: "typeorm:datasource",
   deps: [Logger],
   async useAsyncFactory(logger: Logger) {
-    await postgresDatasource.initialize();
-
-    logger.info("Connected with typeorm to database: Postgres");
-
-    return postgresDatasource;
+    try {
+      await PostgresDatasource.initialize();
+      logger.info("Connected with typeorm to database: Postgres");
+      return PostgresDatasource;
+    } catch (error) {
+      logger.error("Error connecting to database: " + error);
+      throw error;
+    }
   },
   hooks: {
     $onDestroy(dataSource) {
-      return dataSource.isInitialized && dataSource.close();
+      return dataSource.isInitialized && dataSource.destroy();
     }
   }
 });
